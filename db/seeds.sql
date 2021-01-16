c3f7ad8da601ff063b8552050a025ef3cf2f759a
Theresa <TreeGee73@gmail.com>
2021-01-16 11:40:04 -0700

    created db seeds

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+-- Tell MySQL which database to populate with this data
+USE burgers_db;
+
+-- Starter burgers to seed into database
+INSERT INTO
+    bugers (burger_name, devoured)
+VALUES
+    ('Bluecheese Angus Buger', 0),
+    ('California Burger with Avacado', 0),
+    ('Double Cheeseburger', 0),
+    ('Flamethrower Burger', 0),
+    ('Madlove Burger', 0),
+    ('Zen Chicken Burger', 0);
