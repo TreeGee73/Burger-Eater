f2aac9368e52efd23535b11391b61d0eae5e3e71
Theresa <TreeGee73@gmail.com>
2021-01-16 17:33:24 -0700

    first pass server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+// Dependencies
+const express = require('express');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers/burgerController');
+
+const app = express();
+const PORT = process.env.PORT || 6614;
+
+// Middleware
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+// Templates
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Start the server and begin listening for client requests.
+app.listen(PORT, function () {
+    // Server log to indicate the server has started
+  console.log(`Server listening on: http://localhost:${PORT}`);
+});
