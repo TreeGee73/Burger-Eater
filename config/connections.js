f5c0d9b6c8f54da5b430b1cc18cc4e51750a0603
Theresa <TreeGee73@gmail.com>
2021-01-16 17:32:31 -0700

    updated connections.js & main.handlebars

--- config/connections.js
+++ config/connections.js
@@ -4,7 +4,7 @@ const mysql = require('mysql');
 // MySQL connection details/setup
 const connection = mysql.createConnection({
     host: 'localhost',
-    port: 3306,
+    port: 6614,
     user: 'root',
     password: 'root',
     database: 'burgers_db',
