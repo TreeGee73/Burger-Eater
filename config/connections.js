448015edfc42b14155b4063294c2cc2c71f47445
Theresa <TreeGee73@gmail.com>
2021-01-17 22:07:03 -0700

    deployment

--- config/connections.js
+++ config/connections.js
@@ -2,9 +2,11 @@
 const mysql = require('mysql');
 
 // MySQL connection details/setup
-const connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
     host: 'localhost',
-    port: 3306,
     user: 'root',
     password: 'root',
     database: 'burgers_db',
